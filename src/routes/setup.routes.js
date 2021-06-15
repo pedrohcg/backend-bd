899cf2a95c7f3cab9c2c48f5122737ea38ea26b9
pedrohcg <52712904+pedrohcg@users.noreply.github.com>
2021-06-14 21:54:06 -0300

    Update setup.routes.js

--- src/routes/setup.routes.js
+++ src/routes/setup.routes.js
@@ -39,7 +39,7 @@ setupRouter.post('/', async (req, res) => {
             Id_Abertura INT NOT NULL,
             Jogador_Brancas VARCHAR(255) NOT NULL,
             Jogador_Pretas VARCHAR(255) NOT NULL,
-            Quantidade_Movimentos INT NOT NULL,
+            Quantidade_Movimentos VARCHAR(4) NOT NULL,
             Movimentos VARCHAR(4000) NOT NULL,
             CONSTRAINT PK2 PRIMARY KEY CLUSTERED(Id),
             CONSTRAINT FK1 FOREIGN KEY (Id_Abertura) REFERENCES Aberturas(Id)
