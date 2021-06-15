d7a7df6e73ba912d41fef484b9c00de4bc9959ca
Pedro Lucas <plrf100@gmail.com>
2021-06-14 23:21:08 -0300

    feature(cors): adding cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 
 import routes from './routes/index.js';
 
 const app = express();
-
+app.use(cors());
 app.use(routes);
 
-app.listen(3000, () => {  
+app.listen(3333, () => {  
     console.log('Server online');
 })
